56d1ab3da3cf8ccbd3d28420c6b431a5d9eb698f
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-06-04 09:55:12 +0100

    .

--- app/src/components/HogwartsStudents/Student.jsx
+++ app/src/components/HogwartsStudents/Student.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Student(props) {
-    return 'Individual Student!';
+    console.log(props)
+    return (
+        <div className="student-card">
+            <img src={ props.student.img } />
+            <div className="student-info">
+                
+            </div>
+        </div>
+    );
 }
 
 export default Student;
